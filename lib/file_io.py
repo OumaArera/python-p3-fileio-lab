3308d7d7394277938ca2f594b75e5884b502b6b1
OumaArera <johnouma999@gmail.com>
2024-03-06 14:23:08 +0300

    feat: done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    """Write file_content to a .txt file with the given file_name."""
+    file_path = f"lib/{file_name}.txt"
+    with open(file_path, mode="w", encoding="UTF-8") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """Append append_content to a .txt file with the given file_name."""
+    file_path = f"lib/{file_name}.txt"
+    with open(file_path, mode="a", encoding="UTF-8") as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """Read and return the content of a .txt file with the given file_name."""
+    file_path = f"lib/{file_name}.txt"
+    with open(file_path, mode="r", encoding="UTF-8") as file:
+        return file.read()
